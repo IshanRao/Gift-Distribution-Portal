61b96379fde7a93b1a2c6f52621661e425fbfaf7
Ishan <ish.rao36@gmail.com>
2020-01-16 11:17:17 +0530

    Frontend Developed

--- home_app/models.py
+++ home_app/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Gift(models.Model) :
 
-# Create your models here.
+    item = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to='Gifts',blank=True)
+
+class UserProfile(models.Model) :
+
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    address = models.TextField()
+    phone = models.IntegerField(default=0)
+    gift = models.ForeignKey(Gift,on_delete=models.CASCADE)
+
